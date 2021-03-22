e6d4a79ff07c93623ebe51543ac23228b21f043d
AntonShishkin <nuegotonevzlomaut@gmail.com>
2021-03-22 20:43:33 +0300

    add turtle in quest_1.py

--- quest_1.py
+++ quest_1.py
@@ -1,5 +1,5 @@
 import time
-
+import turtle as tr
 
 class TrafficLight():
 
@@ -7,14 +7,34 @@ class TrafficLight():
         self.__color = ""
 
     def running(self):
+        tr.hideturtle()
+        tr.speed('fastest')
         self.__color = "red"
         print(self.__color)
+        tr.down
+        tr.begin_fill()
+        tr.fillcolor(self.__color)
+        tr.circle(50)
+        tr.end_fill()
+        tr.up
         time.sleep(7)
         self.__color = "yellow"
         print(self.__color)
+        tr.down
+        tr.begin_fill()
+        tr.fillcolor(self.__color)
+        tr.circle(50)
+        tr.end_fill()
+        tr.up
         time.sleep(2)
         self.__color = "green"
         print(self.__color)
+        tr.down
+        tr.begin_fill()
+        tr.fillcolor(self.__color)
+        tr.circle(50)
+        tr.end_fill()
+        tr.up
         time.sleep(4)
         self.running()
 
